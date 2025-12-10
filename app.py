e91b2f249a0c81519a77542d7e570078e6a135c4
Đoàn Hồng Bảo <doanhongbao9a@gmail.com>
2025-12-10 20:30:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -348,8 +348,7 @@ def main():
             <div class="footer-title">Dự án Từ điển H're - Việt</div>
             <div class="footer-info">
                 Phát triển bởi <b>Huỳnh Thanh Khải</b><br>
-                Đồng tác giả <b>Đoàn Hồng Bảo</b> <br>
-                Email: huynhthanhkhaibato2011@gmail.com | 24520005@gm.uit.edu.vn
+                Email: huynhthanhkhaibato2011@gmail.com 
             </div>
         </div>
     """, unsafe_allow_html=True)
@@ -357,3 +356,4 @@ def main():
 if __name__ == "__main__":
 
     main()
+
